45222bddc4ab963c4ba23c9319928529549edadb
ti-bui <buimydung2001@gmail.com>
2023-07-21 16:04:31 -0400

    good

--- src/Components/AIprompt/AIprompt.jsx
+++ src/Components/AIprompt/AIprompt.jsx
@@ -1,9 +1,19 @@
 import "./AIprompt.scss"
+import bardIcon from "../../assets/Icons/Bard-logo.svg"
+import {useNavigate} from "react-router-dom"
+
 
 function AIprompt(){
+    const navigate = useNavigate();
     return <>
         <div className="aiprompt">
-            <div className="aiprompt__infolink">Want to learn more about this AI text?</div>
+            <div className="aiprompt__infolink">
+                
+                <p>
+                Want to learn more about this AI text?
+                </p>
+                <img onClick={() => navigate("/info")}  alt="logo" src={bardIcon}/>
+              </div>
             <div className="aiprompt__content">AI has replaced these words: ____</div>
         </div>
     </>
