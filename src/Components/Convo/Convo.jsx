22ca63642a0c64f037099afd53d42bfdd8db7454
Daniel_Martinez <daniel.umanamartinez@gmail.com>
2023-07-21 01:29:20 -0400

    added convo component

---
+++ src/Components/Convo/Convo.jsx
@@ -0,0 +1,31 @@
+import "./Convo.scss"
+
+function Convo(){
+    return (
+        <section className="convo">
+            <div className="right">
+                <div className="convo__text">
+                    <p className="convo__text-content">
+                        Hey what have you been up to?
+                    </p>
+                </div>
+            </div>
+            <div className="left">
+                <div className="convo__text">
+                    <p className="convo__text-content">
+                        not much, same as usual
+                    </p>
+                </div>
+            </div>
+            <div className="right">
+                <div className="convo__text">
+                    <p className="convo__text-content">
+                        Hey what have you been up to?
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Convo;
--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,11 +1,15 @@
 import Header from "../../Components/Header/Header";
 import Textfield from "../../Components/Textfield/Textfield";
+import "./Homepage.scss"
+import Convo from "../../Components/Convo/Convo";
 
 function Homepage(){
     return <>
         <Header/>
+        <Convo/>
         <Textfield/>
-        </>
+    </>
+
 }
 
 export default Homepage;
