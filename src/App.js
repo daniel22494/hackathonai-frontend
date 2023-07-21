22e74497bf8523780a12a3607c6354c7ce50af92
ti-bui <buimydung2001@gmail.com>
2023-07-21 00:18:51 -0400

    Finish second page layout

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage/Homepage";
+import Secondpage from "./Pages/Secondpage/Secondpage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/info" element={<Secondpage />} />
       </Routes>
     </BrowserRouter>
   );
