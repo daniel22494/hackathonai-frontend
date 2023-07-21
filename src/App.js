9c5f0dd255c6a96a336e630c62b3390599302947
ti-bui <buimydung2001@gmail.com>
2023-07-21 00:56:44 -0400

    finish layout for buttonDetails component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage/Homepage";
 import Secondpage from "./Pages/Secondpage/Secondpage";
+import ButtonDetails from "./Components/ButtonDetails/ButtonDetails";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+
         <Route path="/info" element={<Secondpage />} />
+        {/* <Route path="/info" element={<ButtonDetails />} /> */}
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Components/ButtonDetails/ButtonDetails.js
@@ -0,0 +1,40 @@
+const ButtonDetails = () => {
+  return (
+    <section className="main">
+      <header className="main__header">
+        <img alt="back icon" className="main__header-backIcon" />
+        <h2 className="main__header-text">Explore the AI Wonders</h2>
+      </header>
+
+      <button className="main__buttons-button">
+        Lorem Ipsum is simply dummy text ofng?
+      </button>
+
+      <textarea className="main__greeting" placeholder="Placeholder"></textarea>
+
+      <h3 className="main__midText">
+        Lorem ipsum dolor sit lorefkn;awuifha;wuioh.
+      </h3>
+
+      <div className="main__buttons">
+        <button className="main__buttons-button">
+          Lorem ipsum dolor sit amet.
+        </button>
+        <button className="main__buttons-button">
+          Lorem ipsum dolor sit amet.
+        </button>
+        <button className="main__buttons-button">
+          Lorem ipsum dolor sit amet.
+        </button>
+      </div>
+      <div>
+        <input
+          className="main__search"
+          placeholder="Your question here"
+        ></input>
+      </div>
+    </section>
+  );
+};
+
+export default ButtonDetails;
