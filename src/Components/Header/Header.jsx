fe44260ef3f739f54cb6195f436268888daa6c8f
Daniel_Martinez <daniel.umanamartinez@gmail.com>
2023-07-21 13:37:23 -0400

    added icons to header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,17 +1,20 @@
 import "./Header.scss"
+import Back from "../../assets/Icons/back.svg"
+import Video from "../../assets/Icons/video-icon.svg"
+import Phone from "../../assets/Icons/phone-icon.svg"
+import Dots from "../../assets/Icons/3-dot.svg"
 
 function Header(){
     return (
         <header className="header">
             <div className="header__username">
-                <div className="header__pholder"></div>
-                <div className="header__pholder"></div>
+                <img src={Back} alt="back button" className="header__icon" />
                 <h3 className="username__text">Cynthia B.</h3>
             </div>
             <div className="header__actions">
-                <div className="header__pholder"></div>
-                <div className="header__pholder"></div>
-                <div className="header__pholder"></div>
+               <img src={Video} alt="video call button" className="header__icon" />
+               <img src={Phone} alt="back button" className="header__icon" />
+               <img src={Dots} alt="back button" className="header__icon" />
             </div>
         </header>
     )
